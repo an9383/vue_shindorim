c6b7953658617c85bff50811e9a1ee17fe175d0c
an9383 <an9383@gmail.com>
2025-03-15 16:24:47 +0900

    all

--- 0315_03_vuestart_web_cli/app/src/App.vue
+++ 0315_03_vuestart_web_cli/app/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App AN9383"/>
 </template>
 
 <script>
